fb9a4013a5353cf28a67d7e1797b7b59bb2d60c9
wayne <197704620+kaiser498@users.noreply.github.com>
2025-11-20 10:36:50 +0545

    Initial commit

---
+++ main.js
@@ -0,0 +1,75 @@
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 100);
+  let computerChoice;
+  if (randomNumber <= 33) {
+    computerChoice = "rock";
+  } else if (randomNumber <= 66) {
+    computerChoice = "paper";
+  } else if (randomNumber <= 100) {
+    computerChoice = "scissors";
+  }
+  return computerChoice;
+}
+
+function getHumanChoice() {
+  let humanChoice = prompt(
+    "What do you choose? (Rock, Paper, or Scissors)",
+    ""
+  );
+  return humanChoice;
+}
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === "rock" && computerChoice === "paper") {
+      message = "You lose! Paper beats rock.";
+      computerScore++;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+      message = "You win! Rock beats scissors.";
+      humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+      message = "You lose! Scissors beats paper.";
+      computerScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+      message = "You win! Paper beats rock.";
+      humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+      message = "You lose! Rock beats scissors.";
+      computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+      message = "You win! Scissors beats paper.";
+      humanScore++;
+    } else if (humanChoice === computerChoice) {
+      message = "Draw!";
+    }
+    return [alert(message), console.log(message)];
+  }
+
+  playRound(getHumanChoice(), getComputerChoice());
+  console.log("humanScore: " + humanScore, "computerScore: " + computerScore);
+
+  playRound(getHumanChoice(), getComputerChoice());
+  console.log("humanScore: " + humanScore, "computerScore: " + computerScore);
+
+  playRound(getHumanChoice(), getComputerChoice());
+  console.log("humanScore: " + humanScore, "computerScore: " + computerScore);
+
+  playRound(getHumanChoice(), getComputerChoice());
+  console.log("humanScore: " + humanScore, "computerScore: " + computerScore);
+
+  playRound(getHumanChoice(), getComputerChoice());
+  console.log("humanScore: " + humanScore, "computerScore: " + computerScore);
+
+  if (humanScore > computerScore) {
+    message = "Humans now reigns over the computer.";
+  } else {
+    message = "The computer now reigns over humans.";
+  }
+  console.log(message);
+}
+
+playGame();
